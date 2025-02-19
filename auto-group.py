d7e353d482709149ef551d28deb47d94ef3ec7e1
Yves Etter <yves.etter18@gmail.com>
2025-02-19 22:08:06 +0100

    finished auto-group.py and added secrets.py

--- auto-group.py
+++ auto-group.py
@@ -1,7 +1,7 @@
 import os
-
 import discord
 from discord.ext import commands
+from utils.secrets import access_secret_version
 
 # Define intents
 intents = discord.Intents.default()
@@ -9,89 +9,55 @@ intents.voice_states = True
 intents.guilds = True
 intents.messages = True
 
+# Fetch the token from the secret manager
+GCP_PROJECT = "wow-api-441714"
+token = access_secret_version(GCP_PROJECT, "discord-token")
+
 # Initialize the bot
 bot = commands.Bot(command_prefix='!', intents=intents)
-token = os.getenv("DISCORD_TOKEN")  # Replace with your actual bot token
+trigger_sign = '🎧'
 
 @bot.event
 async def on_ready():
     print(f'[{discord.utils.utcnow()}] Connected!')
-
-    # Set the online status.
     await bot.change_presence(status=discord.Status.online)
-
-    # Order the channels.
-    await order_channels()
 
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    # Check if the user entered a new channel.
     if after.channel:
-        # If the user entered a game channel (prefixed with a game controller unicode emoji), group them into their own channel.
-        if after.channel.name.startswith('🎧'):
-            created_channel = await after.channel.clone(name='│ Group', reason="Auto-grouping")
-            await define_channel_pos(after.channel.category)
-            await created_channel.edit(bitrate=96000, position=after.channel.position + 50,
-                                       user_limit=after.channel.user_limit)
+        if after.channel.name.startswith(trigger_sign):
+            new_pos, num = await get_new_channel_properties(after.channel)
+            num_str = f"0{num}" if num <= 9 else f"{num}"
+            created_channel = await after.channel.clone(name=f'│ Gruppe {num_str}', reason="Auto-grouping")
+            await created_channel.edit(position=new_pos, reason="Auto-grouping")
             await member.move_to(created_channel, reason="Auto-grouping")
-            print(
-                f'[{discord.utils.utcnow()}] Moved user "{member.name}" ({member.id}) to voice channel "{created_channel.name}" ({created_channel.id}) at position {created_channel.position}')
+            print(f'[{discord.utils.utcnow()}] Moved user "{member.name}" ({member.id}) to voice channel "{created_channel.name}" ({created_channel.id}) at position {created_channel.position}')
 
-    # Check if the user came from another channel.
     if before.channel:
-        # Delete the user's now empty temporary channel, if applicable.
-        if before.channel.name.startswith('│ Group') and not before.channel.members:
+        if before.channel.name.startswith('│ Gruppe') and not before.channel.members:
             await before.channel.delete(reason="Empty group channel")
             print(f'[{discord.utils.utcnow()}] Deleted voice channel "{before.channel.name}" ({before.channel.id})')
 
 
-@bot.event
-async def on_guild_channel_create(channel):
-    if not channel.name.startswith('│ Group'):
-        await order_channels()
-
-
-@bot.event
-async def on_guild_channel_delete(channel):
-    if not channel.name.startswith('│ Group'):
-        await order_channels()
-
-
-async def define_channel_pos(category):
+async def get_grp_channels(category):
     channel_lst = []
     for channel in category.channels:
-        if channel.name.startswith("│ Group"):
+        if channel.name.startswith("│ Gruppe"):
             channel_lst.append(channel)
-    print(channel_lst)
+    return channel_lst
 
 
-async def order_channels(category):
-    # Get a list of channels.
-    guild = next(iter(bot.guilds), None)  # Assuming the bot is in only one guild for simplicity
-    if not guild:
-        return
-
-    channel_lst = []
-    for channel in category.channels:
-        if channel.type == discord.ChannelType.voice and channel.name.startswith("│ Group"):
-            channel_lst.append(channel)
-
-
-    # Sort channels by their current position.
-    channels_ordered = channel_lst.sort(key=lambda x: x.position)
-    channel_count = len(channels_ordered)
-    channel_numbers = list(range(1, channel_count + 1))
-    channel_extracted_nums = [channel.name.split("│ Group")[1] for channel in channels_ordered]
-
-
-
-
-    # Re-sort channels to support auto-grouping and maximum voice quality.
-    current_position = 100
-    # for channel in channels_ordered:
-    #
-    #     current_position += 100
-
+async def get_new_channel_properties(channel) -> (int, int):
+    grp_channels = await get_grp_channels(channel.category)
+    if grp_channels:
+        channels_ordered = sorted(grp_channels, key=lambda obj: obj.position)
+        for i in range(len(channels_ordered) - 1):
+            if channels_ordered[i].position + 1 != channels_ordered[i + 1].position:
+                return channels_ordered[i].position + 1, int(channels_ordered[i].name.split("Gruppe")[1]) + 1
+        else:
+            return channels_ordered[-1].position + 1, int(channels_ordered[-1].name.split("Gruppe")[1]) + 1
+    else:
+        return channel.position + 1, 1
 
 bot.run(token)
