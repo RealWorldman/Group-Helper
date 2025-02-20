e04eec9f4b579ac119aef40781554a576d997bf9
Yves Etter <yves.etter18@gmail.com>
2025-02-20 01:07:11 +0100

    replace cloud run with vm

--- vm/startup-script.sh
+++ vm/startup-script.sh
@@ -3,7 +3,15 @@
 
 # Update and install necessary packages
 sudo apt-get update
-sudo apt-get install -y python3-pip git
+sudo apt-get install -y wget git
+
+# Install Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# Initialize conda
+source $HOME/miniconda/etc/profile.d/conda.sh
 
 # Clone the repository
 git clone https://github.com/RealWorldman/Group-Helper.git /home/$USER/Group-Helper
@@ -11,8 +19,11 @@ git clone https://github.com/RealWorldman/Group-Helper.git /home/$USER/Group-Hel
 # Navigate to the project directory
 cd /home/$USER/Group-Helper
 
-# Install the Python dependencies
-sudo pip3 install --no-cache-dir -r requirements.txt
+# Create the conda environment
+conda env create -f vm/environment.yml
+
+# Activate the environment
+conda activate discord
 
 # Run the application
 python3 auto-group.py
