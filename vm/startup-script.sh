3291242224768676a81f707c51ed005649bc6bf2
Yves Etter <yves.etter18@gmail.com>
2025-02-20 02:05:27 +0100

    replace cloud run with vm

--- vm/startup-script.sh
+++ vm/startup-script.sh
@@ -25,7 +25,7 @@ conda env create -f environment.yml
 # Activate the environment
 conda activate discord
 
-export GCP_PROJECT=${PROJECT_ID}
+export GCP_PROJECT=$(gcloud config get-value project)
 
 # Run the application
 python3 auto-group.py
