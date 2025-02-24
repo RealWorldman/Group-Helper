ed49d41380aaa6b9e2c197b353d77b30dc4e7238
Yves Etter <yves.etter18@gmail.com>
2025-02-24 23:01:08 +0100

    update vm machines

--- auto-group-app/startup-script_vm_pip.sh
+++ auto-group-app/startup-script_vm_pip.sh
@@ -3,13 +3,16 @@
 
 # Update and install necessary packages
 sudo apt-get update
-sudo apt-get install -y wget git python3-pip
-
-# Clone the repository
-git clone https://github.com/RealWorldman/Group-Helper.git /home/$USER/Group-Helper
+sudo apt-get install -y wget git python3-pip python3-venv
 
 # Navigate to the project directory
 cd /home/$USER/Group-Helper/auto-group-app
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
 
 pip install -r requirements.txt
 
